627aca3c4e1240781fde28d75cb0e617bd024a4c
Alexey Neretin <alexey.neretin@webspellchecker.net>
2025-09-19 18:59:28 +0300

    Remove unnecessary CLI options

--- llmsploit/cli.py
+++ llmsploit/cli.py
@@ -21,13 +21,11 @@ def run(arguments=None):
         parser.add_argument("--target_url", type=str, help="URL address of the investigated LLM")
         parser.add_argument("--target_model_name", type=str, help="Target LLM name")
         parser.add_argument("--target_model_type", type=str, help="Target LLM type", default=argparse.SUPPRESS)
-        parser.add_argument("--target_api_key", type=str, help="Target LLM API key", default=argparse.SUPPRESS)
         parser.add_argument("--categories", type=str, nargs="*", help="Allowed forbidden categories", default=argparse.SUPPRESS)
         parser.add_argument("--exploits", action="store_false", help="Allowed forbidden categories", default=argparse.SUPPRESS)
         parser.add_argument("--evaluation_url", type=str, help="URL address of the evaluation LLM")
         parser.add_argument("--evaluation_model_name", type=str, help="Evaluation LLM name")
         parser.add_argument("--evaluation_model_type", type=str, help="Evaluation LLM type", default=argparse.SUPPRESS)
-        parser.add_argument("--evaluation_api_key", type=str, help="Evaluation LLM API key", default=argparse.SUPPRESS)
 
         args = parser.parse_args(arguments)
         config = vars(args)
